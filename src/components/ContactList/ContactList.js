4dca54c9d5127c865b2f9e23ac2e1664fe1933ea
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-15 12:57:54 +0300

    add list

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,10 +1,18 @@
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from 'redux/contacts/contacts-actions';
+import { getFilteredContacts } from 'redux/contacts/contacts-selectors';
 import s from './ContactList.module.css';
 
-function ContactList({ contactEl, onDeleteContact }) {
+function ContactList() {
+  const contacts = useSelector(getFilteredContacts);
+  const dispatch = useDispatch();
+
+  const onDeleteContact = id => {
+    return dispatch(deleteContact(id));
+  };
   return (
     <ul className={s.list}>
-      {contactEl.map(({ id, name, number }) => (
+      {contacts.map(({ id, name, number }) => (
         <li key={id} className={s.item}>
           <p className={s.text}>
             {name}:{number}
@@ -21,14 +29,5 @@ function ContactList({ contactEl, onDeleteContact }) {
     </ul>
   );
 }
-ContactList.propTypes = {
-  contactEl: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  onDeleteContact: PropTypes.func.isRequired,
-};
+
 export default ContactList;
