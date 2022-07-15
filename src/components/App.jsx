55e8564273d7d4f71f905098771c28205425c046
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-15 14:11:06 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,23 +5,6 @@ import ContactList from './ContactList/ContactList';
 import ContactFilter from './ContactFilter/ContactFilter';
 
 function App() {
-  // const [contacts, setContacts] = useLocalStorage('contacts', contact);
-  // const [filter, setFilter] = useState('');
-
-  // const changeFilter = evt => {
-  //   setFilter(evt.currentTarget.value);
-  // };
-
-  // const contactFormSubmit = contact => {
-  //   contacts.forEach(arr => {
-  //     if (arr.name.toLowerCase() === contact.name.toLowerCase()) {
-  //       alert(`${contact.name} is already in contacts`);
-  //       return;
-  //     }
-  //   });
-  //   dispatch(addContact(contact));
-  // };
-
   return (
     <div className="section">
       <h1>Phonebook</h1>
