e224e689a3ba5c571a115087536de44c32788cfb
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-15 14:11:29 +0300

    storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
-import reduser from './contacts/contacts-reducer';
+import redusers from './contacts/contacts-reducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'items',
+  storage,
+  blacklist: ['filter'],
+};
 
 export const store = configureStore({
   reducer: {
-    contact: reduser,
+    contact: persistReducer(persistConfig, redusers),
   },
+
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
