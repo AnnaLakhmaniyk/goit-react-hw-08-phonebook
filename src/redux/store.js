802df627d9d4f8f780672580901d593892db5918
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-20 23:27:10 +0300

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { rootReducer } from './contacts/contacts-reducer';
+import authSlice from './auth/auth-slice';
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+  blacklist: ['user', 'isLoggedIn'],
+  // whitelist: ['token'],
+};
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    auth: persistReducer(persistConfig, authSlice),
+    contact: rootReducer,
+  },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({ serializableCheck: false }),
+  ],
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
