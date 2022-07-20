ecca368eb15c0cc51501229c2620a3a317a92cf8
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-20 16:07:34 +0300

    list

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,11 +1,19 @@
-import { useDeleteContactMutation } from 'redux/contactsApi';
-import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getContact, deleteContact } from 'redux/contacts/contacts-operation';
+import { getFilteredContacts } from 'redux/contacts/contacts-selectors';
 import { FaTrashAlt } from 'react-icons/fa';
-
 import s from './ContactList.module.css';
 
-function ContactList({ contacts }) {
-  const [deleteContact] = useDeleteContactMutation();
+function ContactList() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getFilteredContacts);
+
+  const onDeleteTodo = id => dispatch(deleteContact(id));
+
+  useEffect(() => {
+    dispatch(getContact());
+  }, [dispatch]);
 
   return (
     <ul className={s.list}>
@@ -17,7 +25,7 @@ function ContactList({ contacts }) {
           <button
             type="button"
             className={s.button}
-            onClick={() => deleteContact(id)}
+            onClick={() => onDeleteTodo(id)}
           >
             Delete <FaTrashAlt size="15" fill="rgb(25, 22, 22)" />
           </button>
@@ -26,14 +34,5 @@ function ContactList({ contacts }) {
     </ul>
   );
 }
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      phone: PropTypes.string.isRequired,
-    })
-  ),
-};
 
 export default ContactList;
