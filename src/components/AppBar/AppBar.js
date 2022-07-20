d33149242aeb0f28c544d196d04ac374f453d197
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-20 23:23:49 +0300

    app bar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,11 +1,15 @@
+import { useSelector } from 'react-redux';
 import AuthNav from './AuthNav';
 import ContactsAdd from './ContactsAdd';
+import UserMenu from './UserMenu';
+import { getIsLoggedIn } from 'redux/auth/auth-selectors';
 import s from './AppBar.module.css';
 
 export default function AppBar() {
+  const isLoggedIn = useSelector(getIsLoggedIn);
   return (
     <header className={s.header}>
-      <AuthNav />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
       <ContactsAdd />
     </header>
   );
