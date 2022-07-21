c019480583d5e41fe28dcff9195daf7702ee5222
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-21 21:39:20 +0300

    contact view

--- src/vievs/ContactView.js
+++ src/vievs/ContactView.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getLoading } from 'redux/contacts/contacts-selectors';
+import { getContact } from 'redux/contacts/contacts-operation';
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 import ContactFilter from 'components/ContactFilter/ContactFilter';
 import Loader from 'components/Loder/Loader';
 
 function ContactView() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContact());
+  }, [dispatch]);
+
   const isLoadingTodos = useSelector(getLoading);
+
   return (
     <div className="section">
       {isLoadingTodos && <Loader />}
