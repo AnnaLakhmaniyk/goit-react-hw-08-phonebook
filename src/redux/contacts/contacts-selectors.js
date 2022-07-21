4d105ac0c14dd71d228e4a5a69ab9a544c23fcc2
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-21 21:38:04 +0300

    contacts-selectors

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,14 +1,3 @@
-import { createSelector } from '@reduxjs/toolkit';
 export const getContacts = state => state.contact.items;
 export const getFilter = state => state.contact.filter;
 export const getLoading = state => state.contact.loadings;
-
-export const getVisibleContact = createSelector(
-  [getContacts, getFilter],
-  (todos, filter) => {
-    const normalizedFilter = filter.toLowerCase();
-    return todos.filter(({ description }) =>
-      description.toLowerCase().includes(normalizedFilter)
-    );
-  }
-);
