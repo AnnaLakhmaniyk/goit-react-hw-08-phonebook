1326a520a2d6592d4927801fd2250bfc3c581594
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-22 14:49:56 +0300

    register view

--- src/vievs/RegisterView.js
+++ src/vievs/RegisterView.js
@@ -1,13 +1,27 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getError } from 'redux/auth/auth-selectors';
+import { toast } from 'react-toastify';
+import { resetError } from 'redux/auth/auh-actions';
 import { register } from 'redux/auth/auth-operations';
 import s from './Vievs.module.css';
 
 export default function RegisterView() {
   const dispatch = useDispatch();
+  const erorSel = useSelector(getError);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (erorSel) {
+      toast.error(`such a user already exists`);
+      navigate('/login');
+      dispatch(resetError());
+    }
+  });
 
   const handleChange = ({ target: { name, value } }) => {
     switch (name) {
